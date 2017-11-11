f93afab08e1bcaeecb70c182f8b85f0f0e61741c
David Welch <dmw9400@gmail.com>
2017-11-11 15:52:40 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(string)
+  string.each do |item|
+    puts item
+  end
 end
 
 def summon_captain_planet# code an argument here
